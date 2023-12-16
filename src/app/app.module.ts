a641791676e8b280e3ea5db1f9fa344dac022ce2
Adrian Dziekonski <adrian.dziekonski@onet.pl>
2023-12-16 16:05:09 +0100

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ValueNGComponent } from './valueNG/valueNG.component';
 import {HttpClientModule} from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-      ValueNGComponent
+      ValueNGComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
